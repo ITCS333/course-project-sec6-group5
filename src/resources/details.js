9b274305f5e1bc4753cbfb9a774a2f0c580c22f8
noor05-ux <nouriix05@gmail.com>
2026-04-29 23:15:03 +0300

    Update details.js

--- src/resources/details.js
+++ src/resources/details.js
@@ -127,7 +127,7 @@ function renderComments() {
  * 6. Call `renderComments()` to refresh the comment list.
  * 7. Clear the textarea.
  */
-function handleAddComment(event) {
+async function handleAddComment(event) {
   // ... your implementation here ...
    event.preventDefault();
 
